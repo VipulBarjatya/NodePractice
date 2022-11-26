66a5c2f2ebee40e505d36254d2fc81e0bec8b7c0
Vipul <vipulbarjatya@gmail.com>
2022-11-26 17:01:42 +0530

    math operations

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const os = require("os");
-const path = require("path");
+// const os = require("os");
+// const path = require("path");
+const { add, subtract, divide, multiply } = require("./math");
 
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
 
-console.log(__dirname);
-console.log(__filename);
+// console.log(__dirname);
+// console.log(__filename);
 
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
 
-console.log(path.parse(__filename));
+// console.log(path.parse(__filename));
+console.log(divide(20, 10));
