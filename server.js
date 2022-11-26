9db0142a0b959b22db11c2b29a2e669b4583a40f
Vipul <vipulbarjatya@gmail.com>
2022-11-26 18:50:40 +0530

    npm modules

--- server.js
+++ server.js
@@ -1,17 +1,5 @@
-// const os = require("os");
-// const path = require("path");
-const { add, subtract, divide, multiply } = require("./math");
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
 
-// console.log(os.type());
-// console.log(os.version());
-// console.log(os.homedir());
-
-// console.log(__dirname);
-// console.log(__filename);
-
-// console.log(path.dirname(__filename));
-// console.log(path.basename(__filename));
-// console.log(path.extname(__filename));
-
-// console.log(path.parse(__filename));
-console.log(divide(20, 10));
+console.log(format(new Date(), "dd-MM-yyyy\tHH:mm:ss"));
+console.log(uuid());
